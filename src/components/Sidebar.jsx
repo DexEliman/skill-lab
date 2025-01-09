65772685eeb3d95ac6ea54e96fc0c53b4f18c5c4
DexEliman <geraume10@gmail.com>
2025-01-09 17:45:40 +0100

    COmit 09012025
    Layout and dashboard

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+// app/components/Sidebar.js
+export default function Sidebar() {
+    return (
+      <aside>
+        <ul>
+          <li><a href="/dashboard">Tableau de bord</a></li>
+          <li><a href="/settings">Paramètres</a></li>
+          <li><a href="/profile">Profil</a></li>
+        </ul>
+      </aside>
+    );
+  }
